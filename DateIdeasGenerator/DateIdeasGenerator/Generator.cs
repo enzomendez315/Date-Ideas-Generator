5465a02ce4e7283fe23c033cd2bbbb9844b9268f
Enzo Mendez <enzo.mendez315@gmail.com>
2022-12-21 16:01:18 -0700

    First commit.

---
+++ DateIdeasGenerator/DateIdeasGenerator/Generator.cs
@@ -0,0 +1,12 @@
+﻿namespace DateIdeasGenerator
+{
+    public class Generator
+    {
+        private List<string> dateIdeas;
+
+        public Generator()
+        {
+            dateIdeas = new List<string>();
+        }
+    }
+}
